06dd5ed7c812f51d5f7837ef3aa20f94bbfe5393
RonanSilasSmith <smith.ronan@outlook.com>
2021-01-13 14:21:29 -0900

    adds MVP for cart component and adds basic outline of cart item component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Nav from "./components/Nav";
-
+import Cart from "./components/Cart";
 
 function App() {
   return (
     <div className="App">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"></link>
       <Nav />
+      <Cart />
     </div>
   );
 }
