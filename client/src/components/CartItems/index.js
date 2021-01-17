2ee84940fe39b82fae34ab0c436a6803e07dd1f4
RonanSilasSmith <smith.ronan@outlook.com>
2021-01-16 23:14:28 -0900

    adds new cart item formatting

--- client/src/components/CartItems/index.js
+++ client/src/components/CartItems/index.js
@@ -4,21 +4,12 @@ import React from "react";
 function CartItems() {
     return (
         <ul class="list-group list-group-flush" style={{ height: "400px", overflow: "auto" }}>
-            <li class="list-group-item">Item1</li>
-            <li class="list-group-item">Item2</li>
-            <li class="list-group-item">Item3</li>
-            <li class="list-group-item">Item1</li>
-            <li class="list-group-item">Item2</li>
-            <li class="list-group-item">Item3</li>
-            <li class="list-group-item">Item1</li>
-            <li class="list-group-item">Item2</li>
-            <li class="list-group-item">Item3</li>
-            <li class="list-group-item">Item1</li>
-            <li class="list-group-item">Item2</li>
-            <li class="list-group-item">Item3</li>
-            <li class="list-group-item">Item1</li>
-            <li class="list-group-item">Item2</li>
-            <li class="list-group-item">Item3</li>
+            <div class="card-body"><li class="list-group-item">
+                <h5 class="card-title">Test Item</h5>
+                <h6 class="card-subtitle text-muted">$Test Price</h6>
+                <p>You have 3 ITEMNAMEs in your cart</p>
+                <a href="#" class="card-link">Remove</a>
+            </li></div>
         </ul>
     )
 }
