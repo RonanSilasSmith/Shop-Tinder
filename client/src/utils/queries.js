feb672a627cc95db3243639a0b0727b0d33f28cc
RonanSilasSmith <smith.ronan@outlook.com>
2021-01-23 20:49:57 -0900

    edit queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,17 @@
 import gql from 'graphql-tag';
 
 export const QUERY_THOUGHTS = gql`
-
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
 `;
