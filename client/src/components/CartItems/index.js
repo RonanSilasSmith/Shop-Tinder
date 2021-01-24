5baddd3581df27ea31922946db9fc7ea5ea05a94
RonanSilasSmith <smith.ronan@outlook.com>
2021-01-23 17:14:25 -0900

    adds a quantity button

--- client/src/components/CartItems/index.js
+++ client/src/components/CartItems/index.js
@@ -2,14 +2,24 @@ import React from "react";
 
 
 function CartItems() {
+    
     return (
         <ul class="list-group list-group-flush" style={{ height: "400px", overflow: "auto" }}>
-            <div class="card-body"><li class="list-group-item">
-                <h5 class="card-title">Test Item</h5>
-                <h6 class="card-subtitle text-muted">$Test Price</h6>
-                <p>You have 3 ITEMNAMEs in your cart</p>
-                <a href="#" class="card-link">Remove</a>
-            </li></div>
+            <div class="card-body">
+                <li class="list-group-item">
+                    <h5 class="card-title">Test Item</h5>
+                    <h6 class="card-subtitle text-muted">$Test Price</h6>
+                    <p><span>Qty:</span>
+                    <input
+                        type="number"
+                        placeholder="1"
+                    />
+                    </p>
+                    <a href="#" class="card-link">Remove</a>
+                </li>
+            
+            </div>
+            
         </ul>
     )
 }
