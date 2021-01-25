aa8b04bd3d7e0ea822e07ef25297ea8fa9adefe8
amokeye <67798512+amokeye@users.noreply.github.com>
2021-01-24 21:30:16 -0800

    working app

--- client/src/components/ProductSelect/index.js
+++ client/src/components/ProductSelect/index.js
@@ -18,41 +18,25 @@ function ProductSelect() {
     console.log(product)
 
     const addItem = () => {
-        
-        // add currentItem to the cart, and change the state of the cart (lines 41 and 42)
-        // temporary post request, it will reset when you refresh
-        //cart.push(stock[itemIndex]);
-        //console.log(cart);
-        //setCart(cart);
-
-        // more permanent
-        // make an axios call to save the cart item to the database
-        // post request
-        // it'll be a permanent change in case someone accidentally refreshes the page or loses internet connection while tindering
-        //alert("added to cart");
-
         moveToNextItem();
     }
 
-
     const moveToNextItem = () => {
-        setCurrentItem(currentItem+1)
+        console.log(product.length)
+        setCurrentItem(currentItem + 1)
+        if (currentItem >= product.length-2){
+            setCurrentItem(0)
+        }
         console.log(currentItem)
     }
 
 
-
     return (
         <main>
-            
             <div className="card centerplace">
                 <Link to={`/products/${product[currentItem]._id}`}>
-                    <img
-                        alt={product[currentItem].name}
-                        src={`/images/${product[currentItem].image}`}
-                    />
                 </Link>
-                <img class="card-img-top" src="https://dummyimage.com/640x360/fff/aaa" alt="Placeholder for products"></img>
+                <img class="card-img-top" src={`/images/${product[currentItem].image}`} alt="Placeholder for products"></img>
                 <div class="card-body">
                     <h5 class="card-title">{product[currentItem].name}</h5>
                     <h6 class="card-subtitle text-muted">${product[currentItem].price}</h6>
