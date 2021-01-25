729dc8582e00ae6ae9c8626e3fcb4ee169b79127
RonanSilasSmith <smith.ronan@outlook.com>
2021-01-24 19:40:33 -0900

    toys around with state, adds a counter to front end

--- client/src/components/ProductSelect/index.js
+++ client/src/components/ProductSelect/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState} from 'react';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { useQuery } from '@apollo/react-hooks';
@@ -7,13 +7,16 @@ import { QUERY_PRODUCTS } from '../../utils/queries'
 
 
 function ProductSelect() {
+    const [currentItem, setCurrentItem] = useState(0);
+
     const [state, dispatch] = useStoreContext();
 
-    const { currentCategory } = state;
-
     const { loading, data } = useQuery(QUERY_PRODUCTS);
-    console.log(data)
 
+    console.log(currentItem)
+    const product = data?.products
+    console.log(product)
+    // const product = data.products
 
     useEffect(() => {
         if (data) {
@@ -23,14 +26,35 @@ function ProductSelect() {
             });
         }
     }, [data, dispatch]);
+    console.log(dispatch)
 
-    function filterProducts() {
-        if (!currentCategory) {
-            return state.products;
-        }
+    // const productList=data.products
+    // console.log(productList)
 
-        return state.products.filter(product => product.category._id === currentCategory);
+    const addItem = () => {
+        
+        // add currentItem to the cart, and change the state of the cart (lines 41 and 42)
+        // temporary post request, it will reset when you refresh
+        //cart.push(stock[itemIndex]);
+        //console.log(cart);
+        //setCart(cart);
+
+        // more permanent
+        // make an axios call to save the cart item to the database
+        // post request
+        // it'll be a permanent change in case someone accidentally refreshes the page or loses internet connection while tindering
+        //alert("added to cart");
+
+        moveToNextItem();
     }
+
+
+    const moveToNextItem = () => {
+        setCurrentItem(currentItem+1)
+        console.log(currentItem)
+    }
+
+
 
     return (
         <main>
@@ -39,11 +63,11 @@ function ProductSelect() {
                 <div class="card-body">
                     <h5 class="card-title">Item Title</h5>
                     <h6 class="card-subtitle text-muted">$Price</h6>
-                    <p class="card-text">Describe the item with however man words.</p>
+                    <p class="card-text">Describe the item with however man words</p>
                     <div className="d-flex justify-content-between">
-                        <a href="/" class="btn btn-primary">Not interested..</a>
-                        <a href="/" class="btn btn-danger">I need this NOW</a>
-                        <a href="/" class="btn btn-primary">Add to cart :)</a>
+                        <button class="btn btn-primary" onClick={moveToNextItem}>Not interested..</button>
+                        <button class="btn btn-danger">I need this NOW</button>
+                        <button class="btn btn-primary" onClick={addItem}>Add to cart :)</button>
                     </div>
                 </div>
             </div>
